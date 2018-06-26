989c5bf1280e08003208f876379ffd443e4f960b
Maciej Znamirowski <maciek@znamirowski.pl>
2018-06-26 15:51:07 +0200

    Loads of changes

--- scripts/menu.js
+++ scripts/menu.js
@@ -3,15 +3,15 @@ window.onscroll = function() {manageNavbarStickness()};
 
 // Get the navbar
 var navbar = document.getElementById('main-menu');
-
-// Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+var mainPageImage = document.getElementById('main-page-image');
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function manageNavbarStickness() {
-  if (window.pageYOffset >= sticky) {
+  if (navbar.getBoundingClientRect().top <= -45 && mainPageImage.getBoundingClientRect().bottom <= 45) {
     navbar.classList.add('sticky-menu')
+    mainPageImage.classList.add('sticky-menu');
   } else {
     navbar.classList.remove('sticky-menu');
+    mainPageImage.classList.remove('sticky-menu');
   }
 } 
